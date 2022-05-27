abf451a324748a8f9f6381c8b191a591212d7085
MuriloFFerreira93 <100986488+MuriloFFerreira93@users.noreply.github.com>
2022-05-27 17:23:19 -0300

    Console App Stopwatch
    Cronômetro básico em console.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set..");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(1000);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++; 
+                Console.WriteLine(currentTime);
+
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!");
+            Thread.Sleep(2500);
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = Char.Parse(data.Substring(data.Length - 1 , 1));
+            int time = int.Parse(data.Substring(0, data.Length > 1 ? data.Length - 1 : data.Length));
+
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+            if (time == 0)
+            {
+                Console.Clear();
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+
+        }
+
+    }
+}
