2f71dcfa86c2b35f23173cb926ba0d5ba04a1ab7
MuriloFFerreira93 <100986488+MuriloFFerreira93@users.noreply.github.com>
2022-07-30 09:07:09 -0300

    1

---
+++ Asp.Net/1-ToDo/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using _1_ToDo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _1_ToDo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ToDoModel> ToDos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+            => options.UseSqlite("DataSource=app.db;Cache:Shared");
+
+    }
+}
